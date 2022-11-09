ee0c6d04c6d95e01878a271cd077d07667e857c0
AdiletAkamtov <adiletakmatovofficial@gmail.com>
2022-11-09 21:59:27 +0600

    refactor: add new section gallery and edit section why

--- js/index.js
+++ js/index.js
@@ -61,3 +61,19 @@ const reviewsSlider = new Swiper('.slider-reviews', {
     },
   
   });
+
+  //------------------------ galary-------------------------//
+
+  const galleryItems = document.querySelectorAll(".gallery__item");
+
+  if(galleryItems.length > 0) {
+    galleryItems.forEach(item => {
+        new Swiper(item, {
+            slidesPerView: 1,
+            autoplay:{
+                delay: 5000,
+            },
+            effect: 'fade',
+        })
+    })
+  }
